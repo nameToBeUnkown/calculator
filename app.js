31cff087e0594455f8c58e865944ac2d918c03eb
DinGis9 <oleg777gorbachov@gmail.com>
2025-01-28 18:00:17 +0200

    add prettier config, format all the files

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
-let currentInput='';let firstOperand='';let currentOperation='';const display=document.getElementById('display');
+let currentInput = ''
+let firstOperand = ''
+let currentOperation = ''
+const display = document.getElementById('display')
 
-function appendNumber(number){currentInput+=number;display.value=currentInput;}
+function appendNumber(number) {
+    currentInput += number
+    display.value = currentInput
+}
 
-function chooseOperation(operation){if(currentInput==='')return;if(firstOperand!==''){calculateResult();}
-firstOperand=currentInput;currentOperation=operation;currentInput='';}
+function chooseOperation(operation) {
+    if (currentInput === '') return
+    if (firstOperand !== '') {
+        calculateResult()
+    }
+    firstOperand = currentInput
+    currentOperation = operation
+    currentInput = ''
+}
 
-function clearDisplay(){currentInput='';firstOperand='';currentOperation='';display.value='';}
+function clearDisplay() {
+    currentInput = ''
+    firstOperand = ''
+    currentOperation = ''
+    display.value = ''
+}
 
-function calculateResult(){if(currentInput===''||firstOperand==='')return;let result;const secondOperand=currentInput;
+function calculateResult() {
+    if (currentInput === '' || firstOperand === '') return
+    let result
+    const secondOperand = currentInput
 
-switch(currentOperation){case'+':result=parseFloat(firstOperand)+parseFloat(secondOperand);break;case'-':result=parseFloat(firstOperand)-parseFloat(secondOperand);break;case'*':result=parseFloat(firstOperand)*parseFloat(secondOperand);break;case'/':result=parseFloat(firstOperand)/parseFloat(secondOperand);break;default:return;}
+    switch (currentOperation) {
+        case '+':
+            result = parseFloat(firstOperand) + parseFloat(secondOperand)
+            break
+        case '-':
+            result = parseFloat(firstOperand) - parseFloat(secondOperand)
+            break
+        case '*':
+            result = parseFloat(firstOperand) * parseFloat(secondOperand)
+            break
+        case '/':
+            result = parseFloat(firstOperand) / parseFloat(secondOperand)
+            break
+        default:
+            return
+    }
 
-display.value=result;currentInput=result.toString();firstOperand='';currentOperation='';}
+    display.value = result
+    currentInput = result.toString()
+    firstOperand = ''
+    currentOperation = ''
+}

12343db105eaf6b6ac5ec3a50457f928fac9c8d7
DinGis9 <oleg777gorbachov@gmail.com>
2025-01-29 10:59:24 +0200

    added prettier config, format all the files

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-let currentInput = ''
-let firstOperand = ''
-let currentOperation = ''
-const display = document.getElementById('display')
+let currentInput = "";
+let firstOperand = "";
+let currentOperation = "";
+const display = document.getElementById("display");
 
 function appendNumber(number) {
-    currentInput += number
-    display.value = currentInput
+    currentInput += number;
+    display.value = currentInput;
 }
 
 function chooseOperation(operation) {
-    if (currentInput === '') return
-    if (firstOperand !== '') {
-        calculateResult()
+    if (currentInput === "") return;
+    if (firstOperand !== "") {
+        calculateResult();
     }
-    firstOperand = currentInput
-    currentOperation = operation
-    currentInput = ''
+    firstOperand = currentInput;
+    currentOperation = operation;
+    currentInput = "";
 }
 
 function clearDisplay() {
-    currentInput = ''
-    firstOperand = ''
-    currentOperation = ''
-    display.value = ''
+    currentInput = "";
+    firstOperand = "";
+    currentOperation = "";
+    display.value = "";
 }
 
 function calculateResult() {
-    if (currentInput === '' || firstOperand === '') return
-    let result
-    const secondOperand = currentInput
+    if (currentInput === "" || firstOperand === "") return;
+    let result;
+    const secondOperand = currentInput;
 
     switch (currentOperation) {
-        case '+':
-            result = parseFloat(firstOperand) + parseFloat(secondOperand)
-            break
-        case '-':
-            result = parseFloat(firstOperand) - parseFloat(secondOperand)
-            break
-        case '*':
-            result = parseFloat(firstOperand) * parseFloat(secondOperand)
-            break
-        case '/':
-            result = parseFloat(firstOperand) / parseFloat(secondOperand)
-            break
+        case "+":
+            result = parseFloat(firstOperand) + parseFloat(secondOperand);
+            break;
+        case "-":
+            result = parseFloat(firstOperand) - parseFloat(secondOperand);
+            break;
+        case "*":
+            result = parseFloat(firstOperand) * parseFloat(secondOperand);
+            break;
+        case "/":
+            result = parseFloat(firstOperand) / parseFloat(secondOperand);
+            break;
         default:
-            return
+            return;
     }
 
-    display.value = result
-    currentInput = result.toString()
-    firstOperand = ''
-    currentOperation = ''
+    display.value = result;
+    currentInput = result.toString();
+    firstOperand = "";
+    currentOperation = "";
 }

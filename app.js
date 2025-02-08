d340a245ace4a0fce847303c5ea57ff3afeba9f8
Andrii Soroka <vt231_sao@student.ztu.edu.ua>
2025-02-08 02:36:35 +0200

    Fixed issue #2: Code Duplication in calculateResult

--- app.js
+++ app.js
@@ -27,24 +27,16 @@ function clearDisplay() {
 
 function calculateResult() {
     if (currentInput === "" || firstOperand === "") return;
-    let result;
-    const secondOperand = currentInput;
+    let num1 = parseFloat(firstOperand);
+    let num2 = parseFloat(currentInput);
 
+    let result;
     switch (currentOperation) {
-        case "+":
-            result = parseFloat(firstOperand) + parseFloat(secondOperand);
-            break;
-        case "-":
-            result = parseFloat(firstOperand) - parseFloat(secondOperand);
-            break;
-        case "*":
-            result = parseFloat(firstOperand) * parseFloat(secondOperand);
-            break;
-        case "/":
-            result = parseFloat(firstOperand) / parseFloat(secondOperand);
-            break;
-        default:
-            return;
+        case "+": result = num1 + num2; break;
+        case "-": result = num1 - num2; break;
+        case "*": result = num1 * num2; break;
+        case "/": result = num1 / num2; break;
+        default: return;
     }
 
     display.value = result;
